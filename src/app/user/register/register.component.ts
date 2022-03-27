b1b34a19f11759bfb8b64d5e6b3b3e01e587e20a
Codenamics <26750398+codenamics@users.noreply.github.com>
2022-03-27 19:48:16 +0200

    custom validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,8 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/servicies/auth.service';
+import { EmailTaken } from '../validators/email-taken';
+import { RegisterValidators } from '../validators/register-validators';
 
 @Component({
   selector: 'app-register',
@@ -11,9 +13,16 @@ import { AuthService } from 'src/app/servicies/auth.service';
 })
 export class RegisterComponent {
   inSubmission = false;
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private emailTaken: EmailTaken
+  ) {}
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
   age = new FormControl('', [
     Validators.required,
     Validators.min(18),
@@ -32,14 +41,17 @@ export class RegisterComponent {
   showAlert = false;
   alertMsg = 'Please wait account is being created';
   alertColor = 'blue';
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
   async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait account is being created';
